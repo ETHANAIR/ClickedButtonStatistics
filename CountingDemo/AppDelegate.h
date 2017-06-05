2bfb722575796039a80de9d39416d870c6105945
Ethan <ethanhuntair@gmail.com>
2017-06-06 00:08:18 +0800

    Initial Commit

---
+++ CountingDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CountingDemo
+//
+//  Created by Ethan on 2017/6/6.
+//  Copyright © 2017年 Ethan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CountingDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CountingDemo
+//
+//  Created by Ethan on 2017/6/6.
+//  Copyright © 2017年 Ethan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
